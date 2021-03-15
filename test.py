6d2ae08a574b725f3e0305a8d8f9a28bc1b05d10
Top <chudeng@hanmail.net>
2021-03-15 17:41:10 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Main.py

--- test.py
+++ test.py
@@ -1,53 +1,71 @@
 infix = '3+1-2*2/2*(3+1*2/2-1)*3-2+1' # result = -15
-'''
-3+1-2*2/2(3+1*2/2-1)*3-2+1
-3+1-((((2*2)/2)(3+((1*2)/2)-1))*3)-2+1
-31222*3122*/1+-/3*21+---+
-'''
+
 answer = list(infix)
 
 def intopost(answer):
     postfix = []
     stk = []
+    print(answer)
     for i in answer:
         if i.isdigit(): # 숫자면 postfix에 i 추가
-             postfix.append(i)
-        elif not i.isdigit():
+            print('digit operated')
+            postfix.append(i)
+        else:   # 연산자이면...
             if i == '(':
+                print('( operated')
                 stk.append(i)   # '('면 stk에 i 추가
             elif i == '*' or i == '/':
+                print('*// operated')
                 if len(stk) == 0 or stk[-1] == '+' or stk[-1] == '-' or stk[-1] == '(':   # stk가 비어 있거나 stk[-1]이 '+' || '-' || '(' 면 stk에 i 추가
                     stk.append(i)
+                    print('*// => if operated')
+                    print(stk)
                 else:
-                    while len(stk) == 0 or stk[-1] == '+' or stk[-1] == '-' or stk[-1] == '(':  # stk[-1]이 *||/ 면, stk[-1]를 postfix로 이동 후 stk에 i 추가
-                        postfix.append(stk.pop(-1))
+                    print('*// => if operated')
+                    while True:
+                        if not len(stk) == 0 or stk[-1] == '+' or stk[-1] == '-' or stk[-1] == '(':  # stk[-1]이 *||/ 면, stk[-1]를 postfix로 이동 후 stk에 i 추가
+                            postfix.append(stk.pop())
+                            continue
+                        else:
+                            break
                     stk.append(i)
+                    print(stk)
             elif i == '+' or i == '-':
+                print('+/- operated')
                 if len(stk) == 0 or stk[-1] == '(':   # stk가 비어 있으면 stk에 i 추가
+                    print('+/- => if operated')
                     stk.append(i)
+                    print(stk)
                 else:
-                    while len(stk) == 0 or stk[-1] == '(':
-                        postfix.append(stk.pop(-1))
+                    print('+/- => else operated')
+                    while True:
+                        if not len(stk) == 0 or stk[-1] == '(':
+                            postfix.append(stk.pop())
+                            continue
+                        else:
+                            break
                     stk.append(i)
+                    print(stk)
             elif i == ')':
                 while stk:
                     if stk[-1] == '(':
                         stk.pop(-1)
                         break
                     else:
-                        postfix.append(stk.pop(-1))
+                        postfix.append(stk.pop())
                         continue
     while stk:
         postfix.append(stk.pop(-1))
     print(postfix, len(postfix))
-    postfix_demo_str = '31+22*2/312*2/+1-*3*-2-1+'
-    postfix_demo = list(postfix_demo_str)
-    return postfix_demo
+    #postfix_demo_str = '31+22*2/312*2/+1-*3*-2-1+'
+    #postfix_demo = list(postfix_demo_str)
+    return postfix
 
 
 # postfix를 받아 계산 후, 결과값은 반환
 def operating(postfix):
     result = []
+    print('postfix of operating', postfix)
     for i in postfix:
         if i.isdigit():
             result.append(int(i))
@@ -55,19 +73,19 @@ def operating(postfix):
         else:
             if i == '+':
                 print(f'sum {result[-1]} + {result[-2]} = {result[-1] + result[-2]} added')
-                sum = result.pop(-1) + result.pop(-1)
+                sum = result.pop() + result.pop()
                 result.append(sum)
             elif i == '-':
                 print(f'sub {result[-1]} - {result[-2]} = {result[-1] - result[-2]} added')
-                sub = result.pop(-1) - result.pop(-1)
+                sub = result.pop() - result.pop()
                 result.append(sub)
             elif i == '*':
                 print(f'mul {result[-1]} x {result[-2]} = {result[-1] * result[-2]} added')
-                mul = result.pop(-1) * result.pop(-1)
+                mul = result.pop() * result.pop()
                 result.append(mul)
             elif i == '/':
                 print(f'dev {result[-1]} / {result[-2]} = {result[-1] / result[-2]} added')
-                dev = result.pop(-1) / result.pop(-1)
+                dev = result.pop() / result.pop()
                 result.append(dev)
     print('result', result, type(result))
 
